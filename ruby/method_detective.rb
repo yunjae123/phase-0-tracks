a605b64df484d1845ab8e020c65b7b2264c4ae4e
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-25 22:45:45 -0700

    find suspects .chop .slice and .delete

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,30 +4,32 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-puts "iNvEsTiGaTiOn".swapcase
-# => “InVeStIgAtIoN”
+# puts "iNvEsTiGaTiOn".swapcase
+# # => “InVeStIgAtIoN”
 
-puts "zom".insert(1, "o")
-puts "zom".insert(2, "o")
-# => “zoom”
+# puts "zom".insert(1, "o")
+# puts "zom".insert(2, "o")
+# # => “zoom”
 
-puts "enhance".center(4 + 7 + 4) #Or just 15 
-# => "    enhance    "
+# puts "enhance".center(4 + 7 + 4) #Or just 15 
+# # => "    enhance    "
 
-puts "Stop! You’re under arrest!".upcase
-# => "STOP! YOU’RE UNDER ARREST!"
+# puts "Stop! You’re under arrest!".upcase
+# # => "STOP! YOU’RE UNDER ARREST!"
 
-puts "the usual".concat(" suspects")
-puts "the usual".insert(-1, " suspects")
-#=> "the usual suspects"
+# puts "the usual".concat(" suspects")
+# puts "the usual".insert(-1, " suspects")
+# #=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+# puts " suspects".insert(0, "the usual")
+# puts " suspects".replace("the usual suspects")
+# # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".slice(1..-1)
+puts "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
 # "Elementary,    my   dear        Watson!".<???>
