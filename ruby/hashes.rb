a363ceb284df05a91dc0e6d92be0d017fcfe45db
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-26 20:26:03 -0700

    Make pseudocode of hashes.rb 5.2

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+Pseudocode
+make hash
+Ask questions
+take some name
+take some age
+take number of children
+take decor theme
+kick some ass
+
+output all their inputs
+ask for edits
+if they say yes, take their new input and replace 
+their old input then print and exit program.
+If not exit the program.
