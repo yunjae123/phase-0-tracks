bcf1981952aab791f594bfa45037318bcf466831
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-26 20:21:10 -0700

    Mess around and explore hashes

--- ruby/test_folder/test_file.rb
+++ ruby/test_folder/test_file.rb
@@ -1,3 +1,9 @@
-puts "What's up"
-answer = gets.chomp
-return answer
+your_hash = {
+	Name: "Yun",
+	Address: "1234 asdf drive",
+	Email: "asdf@yahoo.com",
+	Phone: 123123
+}
+
+puts your_hash[:Name] + " " + your_hash[:Address]
+puts your_hash[:Name]
