c51846d10dd7c73cfdddfd43c8448d567fabcaaf
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-26 20:44:00 -0700

    Add gender and children count

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -15,11 +15,22 @@
 
 def take_input
 	user_info = {}
-	puts "What is the name of the client?(Enter a name)"
+	puts "What is the name of the client? (Enter a name)"
 	user_info[:name] = gets.chomp
 
-	puts "What is the age of client #{user_info[:name]}"
+	puts "What is the client's gender? (Enter M or F)"
+	user_info[:gender] = gets.chomp
+
+
+	puts "What is the age of client #{user_info[:name]}? (Enter a number)"
 	user_info[:age] = gets.chomp
+
+	puts "How many children does #{user_info[:name]} have? (Enter a number)"
+	user_info[:children_count] = gets.chomp
+
+	if user_info[:gender] == "M"
+		puts "What decor theme does Prince #{user_info[:name]} wish to encorporate?"
+
 	user_info
 end
 
