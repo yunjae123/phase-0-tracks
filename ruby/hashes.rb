9a61260177feb5786f4cbd384f337863e577f05d
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-26 20:37:39 -0700

    Ask and take input for questions regarding name and age

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,14 +1,27 @@
-Pseudocode
-make hash
-Ask questions
-take some name
-take some age
-take number of children
-take decor theme
-kick some ass
+# Pseudocode
+# make hash
+# Ask questions
+# take some name
+# take some age
+# take number of children
+# take decor theme
+# kick some ass
 
-output all their inputs
-ask for edits
-if they say yes, take their new input and replace 
-their old input then print and exit program.
-If not exit the program.
+# output all their inputs
+# ask for edits
+# if they say yes, take their new input and replace 
+# their old input then print and exit program.
+# If not exit the program.
+
+def take_input
+	user_info = {}
+	puts "What is the name of the client?(Enter a name)"
+	user_info[:name] = gets.chomp
+
+	puts "What is the age of client #{user_info[:name]}"
+	user_info[:age] = gets.chomp
+	user_info
+end
+
+
+puts take_input;
