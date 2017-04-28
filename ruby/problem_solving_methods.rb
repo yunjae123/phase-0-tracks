991549aabd43ff4b16b98e82d5cff02554e5eeea
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-28 12:11:39 -0700

    Add fibonacci counter and another method that keeps all the fibonacci numbers in an array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -11,4 +11,36 @@ def search_array(array, value)
 return nil 
 end
 
-p search_array(arr, 7)
+# p search_array(arr, 7)
+
+
+def fibonacci(n)
+	if n == 1
+		0
+	elsif n == 2
+		1
+	else
+		fibonacci(n-1) + fibonacci(n-2)
+	end
+end
+
+
+def array_maker(length)
+	new_array = []
+	x = 1
+	while x < length
+		new_array.push(fibonacci(x))
+		x += 1
+	end
+	return new_array
+end
+
+p array_maker(6)
+
+
+
+			
+
+
+
+
