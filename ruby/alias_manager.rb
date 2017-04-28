6ae87c375c3a294e0e7cfea3466fef9f73ad33be
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-27 23:30:34 -0700

    decompose the program into a method that takes in a name and reverses the first and last, and another method that takes a word and changes up vowels and consonants as instructed on 5.5

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,15 +10,34 @@
 def swap_first_last(name) 
 	new_name = name.split(" ").reverse.join(" ")
 	return new_name
+end	
+
+def name_scarmbler(name)
+	vowels = ["a","e","i","o","u"]
+	consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+	new_name = []
+	name_array = name.split("")
+
+	x = 0
+	while x < name_array.length
+		if vowels.index(name_array[x]) != nil
+			if name_array[x] == "u"
+				new_name.push("a")
+			else
+				new_place = vowels.index(name_array[x]) + 1
+				new_name.push(vowels[new_place])
+			end
+		else
+			if name_array[x] == "z"
+				new_name.push("b")
+			else
+				new_place = consonants.index(name_array[x]) + 1
+				new_name.push(consonants[new_place])
+			end
+		end
+		x += 1
+	end
+	new_name.join("")
 end
 
-def change_characters(new_name)
-	vowels = "aeiou"
-	consonants = "bcdfghjklmnpqrstvwxyz"
-	split_name = new_name.split(" ")
-	first_half = split_name[0].split("")
-	second_half = split_name[1].split("")
-
-	p first_half
-end
-puts change_characters("Chang yun")
+puts name_scarmbler("auy")
