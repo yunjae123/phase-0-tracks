c1fae171444d935c2fd94d23653bc196debcabe0
Yun Jae Chang <yunjae0500@yahoo.com>
2017-04-27 23:46:30 -0700

    Make third and last method combining the first two methods and using them to return instructed alias name. Also capitalize the first letters of the new names.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,6 +6,12 @@
 # Have a variable with the consonants
 # Take in first-last reversed name
 # deal with edge cases
+# change letters as instructed
+# shoot out new word
+
+# Pseudocode 
+# Perform last two methods inside and spew out new names
+# Join them together
 
 def swap_first_last(name) 
 	new_name = name.split(" ").reverse.join(" ")
@@ -16,7 +22,7 @@ def name_scarmbler(name)
 	vowels = ["a","e","i","o","u"]
 	consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
 	new_name = []
-	name_array = name.split("")
+	name_array = name.downcase.split("")
 
 	x = 0
 	while x < name_array.length
@@ -37,7 +43,19 @@ def name_scarmbler(name)
 		end
 		x += 1
 	end
-	new_name.join("")
+	return new_name.join("")
 end
 
-puts name_scarmbler("auy")
+def alias_manager(first_last)
+	new_name = swap_first_last(first_last).split(" ")
+	first_name = new_name[0]
+	last_name = new_name[1]
+
+	new_first = name_scarmbler(first_name)
+	new_last = name_scarmbler(last_name)
+	upcased_first = new_first.slice(0).upcase + new_first.slice(1..-1)
+	upcased_last = new_last.slice(0).upcase + new_last.slice(1..-1)
+
+	return upcased_first + " " + upcased_last
+end
+p alias_manager("Yun chang")
