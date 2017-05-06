9dfa4ea032bc67aeb4ac9d7bd845e6fbe8de6c32
Yun Jae Chang <yunjae0500@yahoo.com>
2017-05-05 20:04:39 -0700

    Add method that increases santas age by one. Add method that takes inputted reindeer name and finds that name in the reindeer rankings array, and repositions it at the end index of the array.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,7 +15,27 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.push(reindeer_name)
+	end
+
 end
+
+
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+# 	santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
 
 
 
