dbf4db79c57f9569b51b3a1397a4f8c90035cc39
Yun Jae Chang <yunjae0500@yahoo.com>
2017-05-05 19:40:41 -0700

    Add attributes like age, gender, ranking, and name

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -7,12 +15,11 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
-
-	def initialize
-		puts "Initializing Santa instance..."
-	end
-
 end
 
-old_beard = Santa.new
-old_beard.speak
+
+
+# old_beard = Santa.new
+# old_beard.speak
+# old_beard.eat_milk_and_cookies("oatmeal")
+
