832de76c3f3c03ee35cfd5d14f6452f31bd98516
Yun Jae Chang <yunjae0500@yahoo.com>
2017-05-12 21:52:29 -0700

    Add function that adds new name and color into each arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+colorArray = ["blue", "red", "black", "white"]
+nameArray = ["Ed", "Edd", "Edison", "Eddy"]
+
+function addColorAndName(color, name) {
+	colorArray.push(color)
+	nameArray.push(name)
+}
+
+
