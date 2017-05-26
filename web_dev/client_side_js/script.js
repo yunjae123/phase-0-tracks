bbf98b11da5cea738c5940a67449d9301ad0e7cb
Yun Jae Chang <yunjae0500@yahoo.com>
2017-05-26 13:18:56 -0700

    Finish 9.2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -7,7 +7,7 @@ li.style.border = "10px solid pink"
 
 var photo = document.getElementById("lizard-photo")
 
-function hidePhoto() {
+function hidePhoto(event) {
 	photo.hidden = true;
 }
 li.addEventListener("click", hidePhoto)
